b2f42c6894cea83208dedcc7d4ce8f6228007fc7
Campe01 <81365757+Campe01@users.noreply.github.com>
2021-03-25 13:26:05 +0100

    Add files via upload

---
+++ HTML5Application/public_html/js/main_1.js
@@ -0,0 +1,44 @@
+
+var playerContainer = document.getElementById("player-info");
+var btn = document.getElementById("btn");
+
+btn.addEventListener("click", function() {
+	
+  var ourRequest = new XMLHttpRequest();
+  ourRequest.open('GET', 'http://data.nba.net/10s/prod/v1/2018/players.json');
+  ourRequest.onload = function() {
+    if (ourRequest.status >= 200 && ourRequest.status < 400) {
+      var ourData = JSON.parse(ourRequest.responseText);
+      renderHTML(ourData);
+	  console.log(ourData);
+    } else {
+      console.log("We connected to the server, but it returned an error.");
+    }
+
+  };
+
+  ourRequest.onerror = function() {
+    console.log("Connection error");
+  };
+
+  ourRequest.send();
+  //pageCounter++;
+  //if (pageCounter > 3) {
+  btn.classList.add("hide-me");
+  //}
+});
+
+function renderHTML(data) {
+  var htmlString = "";
+	
+  for (i = 0; i < 50; i++) {
+    htmlString += "<p>" + data.league.vegas[i].firstName+" "+data.league.vegas[i].lastName+" numero maglia:"+data.league.vegas[i].jersey ;
+	
+    htmlString += '</p>';
+
+  }
+  console.log(htmlString);
+
+  playerContainer.insertAdjacentHTML('beforeend', htmlString);
+}
+
